c57117f137b2b8dbd1831b19376d38574d23a73a
alfredobs97 <alfredobautista1@gmail.com>
2019-12-06 12:53:46 +0100

    change layout card, no text overflowed

--- lib/mini-gift.dart
+++ lib/mini-gift.dart
@@ -15,24 +15,42 @@ class _MiniGiftState extends State<MiniGift> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Stack(
-        alignment: AlignmentDirectional.topEnd,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Positioned.fill(
-            child: Image.network(widget.img, fit: BoxFit.fill),
+          Expanded(
+                      child: ClipRRect(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                child: Image.network(widget.img, fit: BoxFit.fill),
+            ),
           ),
-          IconButton(
-            icon: Icon(Icons.favorite),
-            color: widget.isSelected ? Colors.red : Colors.white,
-            onPressed: (){
-              setState(() {
-                widget.isSelected ? widget.isSelected = false : widget.isSelected = true;
-              });              
-            },
-          )
+          ClipRRect(
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+            child: Container(
+              color: Colors.blue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    widget.nameGift,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.favorite),
+                    color: widget.isSelected ? Colors.red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        widget.isSelected ? widget.isSelected = false : widget.isSelected = true;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       elevation: 5,
       margin: EdgeInsets.all(10),
     );
