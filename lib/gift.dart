6de2c25efb3a149782247e3a654c07ab9263cef4
alfredobs97 <alfredobautista1@gmail.com>
2019-12-06 18:49:15 +0100

    color filter gift

--- lib/gift.dart
+++ lib/gift.dart
@@ -10,23 +10,46 @@ class Gift extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.all(Radius.circular(40)),
-      child: Container(
-        height: MediaQuery.of(context).size.height / 4,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.blue,
-        child: Column(
-          children: <Widget>[
-            Image.network(
-              img,
-              height: 200,
-            ),
-            Text(
-              nameGift,
-              style: TextStyle(fontSize: 20),
-            )
-          ],
+      child: Stack(children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 4,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(img), fit: BoxFit.fill),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  nameGift,
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+        Container(
+          height: MediaQuery.of(context).size.height / 4,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [
+                0.4,
+                1
+              ],
+              colors: [
+                Theme.of(context).backgroundColor.withOpacity(0.0),
+                Theme.of(context).primaryColor
+              ]
+            ),
+          ),
+        ),
+
+      ]),
     );
   }
 }
